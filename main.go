27f8962542beeddb80151e3094a2f7e863917584
Long Kiều Tuấn <longkt@smartosc.com>
2021-07-22 22:47:14 +0700

    update code

--- main.go
+++ main.go
@@ -9,6 +9,83 @@ import (
 	"net/url"
 )
 
+func giaiThua(n int) int {
+	if (n == 1) {
+		return n
+	}
+	return n * giaiThua(n-1)
+}
+
+func soHoanHao(n int) bool  {
+	sum := 0
+	for i :=0; i<n; i++ {
+		if sum % i == 0 {
+			sum += i
+		}
+	}
+	if sum == n {
+		return true
+	}
+	return false
+}
+
+////xoa ki tu cua 1 chuoi
+//func xoaKiTu(a string, c string) string {
+//	strLen := len(a)
+//	for i := 0; i< strLen; i++ {
+//		for j := i; j<strLen; j ++ {
+//			if a[i] == c[0] {
+//				a[j] = a[j+1]
+//				a[strLen] = '\0'
+//				i--
+//			}
+//		}
+//	}
+//}
+//
+//func xoaKiTu(a string, c string) string {
+//	//mapp := make(map[string]string)
+//	//for k, v := range mapp {
+//	//
+//	//}
+//	//xoa ki tu c trong chuoi a
+//	return strings.Replace(a, c, "", -1)
+//}
+//
+//func getNumber(s string) (string, string) {
+//	//xoa tung ki tu lan luot trong chuoi, luu duoi dang map
+//	//tim ra value nho nhat trong map
+//	//tu value -> key
+//	var strMap = make(map[string]string)
+//	for i := 0; i < len(s); i ++ {
+//		var strNew = xoaKiTu(s, string(s[i]))
+//		strMap[string(s[i])] = strNew
+//	}
+//
+//	min,_ := strconv.Atoi(strMap[string(s[0])])
+//	minChar := string (s[0])
+//	for key, value := range strMap {
+//		num, _ := strconv.Atoi(value)
+//		if num < min {
+//			min = num
+//			minChar = string(key)
+//		}
+//	}
+//	return fmt.Sprintf("%d", min), minChar
+//}
+
+
+//func chuoiNhoNhat(a string) string {
+//	lengthStr := len(a)
+//	arrNumDeleted := make([]string, 100000000)
+//	for i := 0; i < lengthStr; i++ {
+//		if a[i] == a[i+1] {
+//
+//		}
+//
+//	}
+//}
+
 func main() {
 	routes := chi.NewRouter()
 	r := graphql.RegisterRoutes(routes)
